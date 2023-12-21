f94ca2c8196458b906dba8f5f2e7033b861a72a8
Mark Kelsall <99473523+geekissh@users.noreply.github.com>
2023-12-21 17:03:15 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const mainElement = document.querySelector('main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "YOUR-NAME is the champion";
+
+document.body.appendChild(newHeader);
